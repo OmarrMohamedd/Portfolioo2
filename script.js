96670a4150211b237806427241b4d43f258f5421
OmarrMohamedd <120760006+OmarrMohamedd@users.noreply.github.com>
2023-07-17 00:31:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/* === typing animation === */
+
+var typed = new Typed(".typing", {
+ /* strings:[" " , "Web Designer", "Web Developer", "Graphic Design"],*/
+  strings:[" " ,"لفاجر ", " القادر", " المطرشم ", " ابو جلابية اللي ما بيشوفش"],
+  typeSpeed:100,
+  BackSpeed:60,
+  loop:true
+})
